9512266429e6fe220cf07d16fcc37e9a07b0c06b
AndrewYinLi <ali@oberlin.edu>
2019-05-08 16:13:26 -0700

    Hello, World

---
+++ chapter1/main.go
@@ -0,0 +1,8 @@
+package main // Package declaration, every Go program must start with it.
+
+import "fmt" // shorthand for format, implements formatting for I/O
+
+func main(){
+    // Access man pages: `go doc fmt.Println`
+    fmt.Println("Hello, World") // If Go doesn't even use semi-colons, why not permit printing with "print(...)" -_-
+}
